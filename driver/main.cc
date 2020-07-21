17aa5e3d1ffc7df0a7abcd22c570026911876754
Owen Smith <ojman101@gmail.com>
2020-07-21 18:27:14 +0100

    driver: Use InputFile

--- driver/main.cc
+++ driver/main.cc
@@ -1,7 +1,8 @@
-#include <fstream>
+#include <bamf/io/InputFile.hh>
+
 #include <iostream>
-#include <stdexcept>
-#include <vector>
+
+using namespace bamf;
 
 namespace {
 
@@ -14,21 +15,12 @@ void print_usage(const char *prog_name) {
 int main(int argc, char **argv) {
     if (argc != 2) {
         print_usage(argv[0]);
-        return -1;
+        return 1;
     }
-
-    std::ifstream file(argv[1], std::ios::ate | std::ios::binary);
-    if (!file) {
-        throw std::runtime_error("Failed to open file " + std::string(argv[1]) + "!");
-    }
-
-    const auto size = file.tellg();
-    std::vector<char> data(size);
-    file.seekg(0);
-    file.read(data.data(), size);
 
     std::cerr << "Parsing: " << std::hex;
-    for (char ch : data) {
+    InputFile input(argv[1]);
+    for (char ch : input) {
         std::cerr << (static_cast<int>(ch) & 0xFF) << ' ';
     }
     std::cerr << '\n';
