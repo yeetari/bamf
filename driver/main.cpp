da3e14b8911e2b473fbe6aab4e65d948f65fb947
Owen Smith <ojman101@gmail.com>
2020-07-21 17:49:11 +0100

    driver: Take in file as argument and dump bytes

--- driver/main.cpp
+++ driver/main.cpp
@@ -1,5 +1,35 @@
+#include <fstream>
 #include <iostream>
+#include <stdexcept>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, world!\n";
+namespace {
+
+void print_usage(const char *prog_name) {
+    std::cerr << "Usage: " << prog_name << " <file>\n";
+}
+
+} // namespace
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        print_usage(argv[0]);
+        return -1;
+    }
+
+    std::ifstream file(argv[1], std::ios::ate | std::ios::binary);
+    if (!file) {
+        throw std::runtime_error("Failed to open file " + std::string(argv[1]) + "!");
+    }
+
+    const auto size = file.tellg();
+    std::vector<char> data(size);
+    file.seekg(0);
+    file.read(data.data(), size);
+
+    std::cerr << "Parsing: " << std::hex;
+    for (char ch : data) {
+        std::cerr << (static_cast<int>(ch) & 0xFF) << ' ';
+    }
+    std::cerr << '\n';
 }
