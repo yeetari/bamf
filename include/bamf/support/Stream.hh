6b0d6afedd3b3485c5e61cace1fd2298934033fa
Owen Smith <ojman101@gmail.com>
2020-07-23 19:52:35 +0100

    support: Add Stream class

---
+++ include/bamf/support/Stream.hh
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <bamf/core/NonCopyable.hh>
+#include <bamf/core/NonMovable.hh>
+
+#include <cstdint>
+
+namespace bamf {
+
+class Stream {
+    const char *const m_data;
+    const std::size_t m_length;
+    std::size_t m_bytes_read{0};
+
+public:
+    BAMF_MAKE_NON_COPYABLE(Stream)
+    BAMF_MAKE_NON_MOVABLE(Stream)
+
+    constexpr Stream(const char *data, std::size_t length) : m_data(data), m_length(length) {}
+    ~Stream() = default;
+
+    template <typename T>
+    T read();
+
+    bool has_more() const { return m_bytes_read < m_length; }
+    std::size_t bytes_read() const { return m_bytes_read; }
+};
+
+} // namespace bamf
---
+++ src/support/Stream.cc
@@ -0,0 +1,22 @@
+#include <bamf/support/Stream.hh>
+
+#include <cstdint>
+
+namespace bamf {
+
+template <>
+std::uint8_t Stream::read() {
+    return static_cast<std::uint8_t>(m_data[m_bytes_read++]);
+}
+
+template <>
+std::uint16_t Stream::read() {
+    return read<std::uint8_t>() | (read<std::uint8_t>() << 8U);
+}
+
+template <>
+std::uint32_t Stream::read() {
+    return read<std::uint16_t>() | (read<std::uint16_t>() << 16U);
+}
+
+} // namespace bamf
