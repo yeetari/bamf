02d135aec0a53c4a09a138de52d528e068b74943
Owen Smith <ojman101@gmail.com>
2020-07-27 17:35:59 +0100

    support: Make BAMF_MAKE_ITERABLE work on dependant types

--- include/bamf/support/Iterator.hh
+++ include/bamf/support/Iterator.hh
@@ -2,8 +2,8 @@
 
 // TODO(): reverse_iterator and const_reverse_iterator
 #define BAMF_MAKE_ITERABLE(list) \
-    using iterator = decltype(list)::iterator; \
-    using const_iterator = decltype(list)::const_iterator; \
+    using iterator = typename decltype(list)::iterator; \
+    using const_iterator = typename decltype(list)::const_iterator; \
     iterator begin() { return list.begin(); } \
     iterator end() { return list.end(); } \
     const_iterator begin() const { return list.begin(); } \
