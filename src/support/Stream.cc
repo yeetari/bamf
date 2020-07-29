33cb5e32e2873d79fbd1c0600aff8a1aa97667a5
Owen Smith <ojman101@gmail.com>
2020-07-29 19:24:21 +0100

    support: Silence some warnings in Stream

--- src/support/Stream.cc
+++ src/support/Stream.cc
@@ -11,12 +11,13 @@ std::uint8_t Stream::read() {
 
 template <>
 std::uint16_t Stream::read() {
-    return read<std::uint8_t>() | (read<std::uint8_t>() << 8U);
+    return read<std::uint8_t>() | (static_cast<std::uint16_t>(read<std::uint8_t>()) << 8U);
 }
 
 template <>
 std::uint32_t Stream::read() {
-    return read<std::uint16_t>() | (read<std::uint16_t>() << 16U);
+    return read<std::uint16_t>() | (static_cast<std::uint32_t>(read<std::uint16_t>()) << 16U);
+}
 }
 
 } // namespace bamf
