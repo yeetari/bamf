68af3fb2436d636be813afdd5fdba30fa85e9ddf
Owen Smith <ojman101@gmail.com>
2020-07-30 19:46:18 +0100

    ir: Add is<T>() and as<T>() functions to Expression and Statement

--- include/bamf/ir/Expression.hh
+++ include/bamf/ir/Expression.hh
@@ -5,6 +5,22 @@ namespace bamf {
 struct Expression {
     virtual ~Expression() = default;
     virtual void dump() const = 0;
+
+    template <typename T>
+    T *as();
+
+    template <typename T>
+    bool is();
 };
+
+template <typename T>
+T *Expression::as() {
+    return dynamic_cast<T *>(this);
+}
+
+template <typename T>
+bool Expression::is() {
+    return as<T>() != nullptr;
+}
 
 } // namespace bamf
