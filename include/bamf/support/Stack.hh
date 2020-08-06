7e70fa73e58f9e26e76600bc3200e8c781e8c232
Owen Smith <ojman101@gmail.com>
2020-08-07 00:40:59 +0100

    support: Add Stack<T>

---
+++ include/bamf/support/Stack.hh
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <bamf/support/Iterator.hh>
+#include <bamf/support/NonCopyable.hh>
+#include <bamf/support/NonMovable.hh>
+
+#include <cstdint>
+#include <vector>
+
+namespace bamf {
+
+template <typename T>
+class Stack {
+    std::vector<T *> m_impl;
+
+public:
+    BAMF_MAKE_ITERABLE(m_impl)
+    BAMF_MAKE_NON_COPYABLE(Stack)
+    BAMF_MAKE_NON_MOVABLE(Stack)
+
+    Stack() = default;
+    ~Stack() = default;
+
+    constexpr void push(T *value);
+    constexpr T *peek() const;
+    constexpr T *pop();
+
+    constexpr bool empty() const { return m_impl.empty(); }
+    constexpr std::size_t size() const { return m_impl.size(); }
+};
+
+template <typename T>
+constexpr void Stack<T>::push(T *value) {
+    m_impl.push_back(value);
+}
+
+template <typename T>
+constexpr T *Stack<T>::peek() const {
+    return m_impl.back();
+}
+
+template <typename T>
+constexpr T *Stack<T>::pop() {
+    auto *ret = m_impl.back();
+    m_impl.pop_back();
+    return ret;
+}
+
+} // namespace bamf
---
+++ tests/support/StackTest.cc
@@ -0,0 +1,29 @@
+#include <bamf/support/Stack.hh>
+
+#include <gtest/gtest.h>
+
+using namespace bamf;
+
+namespace {
+
+TEST(StackTest, PushPeekPop) {
+    auto *elem_one = new int(5);
+    auto *elem_two = new int(6);
+
+    Stack<int> stack;
+    stack.push(elem_one);
+    stack.push(elem_two);
+    EXPECT_EQ(stack.size(), 2);
+    EXPECT_FALSE(stack.empty());
+
+    EXPECT_EQ(stack.peek(), elem_two);
+    EXPECT_EQ(stack.pop(), elem_two);
+    EXPECT_EQ(stack.size(), 1);
+    EXPECT_FALSE(stack.empty());
+    EXPECT_EQ(stack.peek(), elem_one);
+    EXPECT_EQ(stack.pop(), elem_one);
+    EXPECT_EQ(stack.size(), 0);
+    EXPECT_TRUE(stack.empty());
+}
+
+} // namespace
