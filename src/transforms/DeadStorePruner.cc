2ac03f7e6d3182f99c5cde846d3c15833f3a9702
Owen Smith <ojman101@gmail.com>
2020-08-09 16:59:15 +0100

    dead-store-pruner: Various fixes

--- src/transforms/DeadStorePruner.cc
+++ src/transforms/DeadStorePruner.cc
@@ -6,7 +6,7 @@
 #include <bamf/ir/Instructions.hh>
 
 #include <cassert>
-#include <unordered_map>
+#include <map>
 #include <vector>
 
 namespace bamf {
@@ -18,7 +18,7 @@ struct VarInfo {
     std::vector<StoreInst *> stores;
 };
 
-void run(BasicBlock *block, std::unordered_map<Value *, VarInfo> *map, int *pruned_count) {
+bool run(BasicBlock *block, std::map<Value *, VarInfo> *map, int *pruned_count) {
     // Build def-use info
     auto &info_map = *map;
     for (auto &inst : *block) {
@@ -29,6 +29,7 @@ void run(BasicBlock *block, std::unordered_map<Value *, VarInfo> *map, int *prun
         }
     }
 
+    bool changed = false;
     for (auto &[var, info] : info_map) {
         // If a var only has one store (def), we can propagate the load values with the store value
         // NOTE: The dead loads will stay after this pass, you must run the TriviallyDeadInstPruner pass
@@ -36,24 +37,38 @@ void run(BasicBlock *block, std::unordered_map<Value *, VarInfo> *map, int *prun
             auto *store = info.stores[0];
             for (auto *load : info.loads) {
                 // Propagate all uses of the load with the RHS of the store
+                changed = true;
                 load->replace_all_uses_with(store->src());
+                var->remove_user(load);
             }
+
+            // Something other than the loads/store is using this var, we can't remove it
+            if (var->users().size() >= 2) {
+                continue;
+            }
+
             // Remove dead store
             assert(store->uses().empty());
             block->remove(store);
+            changed = true;
             (*pruned_count)++;
         }
     }
+    return changed;
 }
 
 } // namespace
 
 void DeadStorePruner::run_on(Function *function) {
-    std::unordered_map<Value *, VarInfo> info_map;
     int pruned_count = 0;
-    for (auto &block : *function) {
-        run(block.get(), &info_map, &pruned_count);
-    }
+    bool changed = false;
+    do {
+        changed = false;
+        std::map<Value *, VarInfo> info_map;
+        for (auto &block : *function) {
+            changed |= run(block.get(), &info_map, &pruned_count);
+        }
+    } while (changed);
     m_logger.trace("Pruned {} dead stores", pruned_count);
 }
 
