cc41ef798c5f37c2799d0a6593f01afe79403754
Owen Smith <ojman101@gmail.com>
2020-08-11 16:48:03 +0100

    alloc-promoter: Major cleanup

--- src/transforms/AllocPromoter.cc
+++ src/transforms/AllocPromoter.cc
@@ -15,81 +15,68 @@ namespace bamf {
 namespace {
 
 struct VarInfo {
-    std::vector<StoreInst *> defs;
-    std::vector<LoadInst *> uses;
-    std::vector<Instruction *> instructions;
+    Stack<Value> def_stack;
+    std::vector<StoreInst *> stores;
 };
 
-void promote_single_store(const VarInfo &info, int *propagated_load_count) {
-    auto *store = info.defs[0];
-    for (auto *load : info.uses) {
-        auto *block = load->parent();
-        if (block != store->parent()) {
-            continue;
+bool is_promotable(AllocInst *alloc) {
+    for (auto *user : alloc->users()) {
+        if (auto *store = user->as<StoreInst>()) {
+            if (store->dst() != alloc) {
+                return false;
+            }
         }
-
-        // Don't propagate loads that are before the store
-        if (std::distance(block->position_of(load), block->position_of(store)) > 0) {
-            continue;
-        }
-
-        assert(store->src() != load);
-        load->replace_all_uses_with(store->src());
-        (*propagated_load_count)++;
     }
-
-    assert(store->users().empty());
-    store->remove_from_parent();
+    return true;
 }
 
 } // namespace
 
-// TODO: Rework some of the terminology here? A use in def-use info could be confused with a value's uses.
 void AllocPromoter::run_on(Function *function) {
-    // Build def-use (store-load) info.
+    // Build store-load (def-use) info
     std::unordered_map<AllocInst *, VarInfo> vars;
+    std::unordered_map<Value *, AllocInst *> reverse_map;
     for (auto &block : *function) {
         for (auto &inst : *block) {
-            if (auto *load = inst->as<LoadInst>()) {
-                if (auto *alloc = load->ptr()->as<AllocInst>()) {
-                    vars[alloc].uses.push_back(load);
-                    vars[alloc].instructions.push_back(load);
-                }
-            } else if (auto *store = inst->as<StoreInst>()) {
-                if (auto *alloc = store->dst()->as<AllocInst>()) {
-                    vars[alloc].defs.push_back(store);
-                    vars[alloc].instructions.push_back(store);
+            auto *alloc = inst->as<AllocInst>();
+            if (alloc == nullptr) {
+                continue;
+            }
+
+            // Ignore dead variable. If it trivially dead (i.e. no uses at all), it will be removed by the
+            // TriviallyDeadInstPruner pass.
+            if (alloc->users().empty()) {
+                continue;
+            }
+
+            // Some variables can't be promoted, e.g. if its address is taken (it is on the RHS of a store).
+            if (!is_promotable(alloc)) {
+                continue;
+            }
+
+            assert(!vars.contains(alloc));
+            for (auto *user : alloc->users()) {
+                reverse_map[user] = alloc;
+                if (auto *store = user->as<StoreInst>()) {
+                    vars[alloc].stores.push_back(store);
                 }
             }
         }
     }
 
     int propagated_load_count = 0;
-    int pruned_store_count = 0;
-    for (auto &[var, info] : vars) {
-        // Ignore dead variable. If it trivially dead (i.e. no uses at all), it will be removed by the
-        // TriviallyDeadInstPruner pass.
-        if (info.defs.empty()) {
-            continue;
-        }
+    for (auto &block : *function) {
+        for (auto &inst : *block) {
+            if (!reverse_map.contains(inst.get())) {
+                continue;
+            }
 
-        // If a var only has one definition (store), we can propagate all uses of the loads with the RHS of the store.
-        // Dead allocs or loads are not pruned by this pass. Normally you would want a dead instruction pruner pass to
-        // run sometime after this.
-        if (info.defs.size() == 1) {
-            promote_single_store(info, &propagated_load_count);
-            pruned_store_count++;
-            continue;
-        }
-
-        // Else we have to fall back to standard SSA construction. Note that this pass does not modify any memory
-        // accesses explicitly, it simply promotes trivially propagatable allocs. Bamf IR is similar to LLVM in that SSA
-        // form is only used for scalar registers (and not memory).
-        Stack<Value> def_stack;
-        for (auto *inst : info.instructions) {
+            // TODO: Avoid allocating a Stack<Value> for single store allocs
+            auto &info = vars.at(reverse_map.at(inst.get()));
+            auto &def_stack = info.def_stack;
             if (auto *load = inst->as<LoadInst>()) {
                 if (!def_stack.empty()) {
-                    // Load will become trivially dead.
+                    // Load will become trivially dead
                     load->replace_all_uses_with(def_stack.peek());
                     propagated_load_count++;
                 }
@@ -97,10 +84,13 @@ void AllocPromoter::run_on(Function *function) {
                 def_stack.push(store->src());
             }
         }
+    }
 
+    int pruned_store_count = 0;
+    for (auto &[var, info] : vars) {
         // Remove (now dead) stores. We have to do this here, since the dead instruction pruner pass won't be able to
         // tell that this store is dead (since it technically still has uses).
-        for (auto *store : info.defs) {
+        for (auto *store : info.stores) {
             assert(store->users().empty());
             store->remove_from_parent();
             pruned_store_count++;
