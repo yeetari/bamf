f72409bc67e17a792907536785d4eb90a0a6d97b
Owen Smith <ojman101@gmail.com>
2020-08-16 20:24:49 +0100

    cfg-simplifier: Re-construct CFG every iteration

--- src/transforms/CfgSimplifier.cc
+++ src/transforms/CfgSimplifier.cc
@@ -10,13 +10,29 @@ namespace bamf {
 
 namespace {
 
-bool run(Function *function, Graph<BasicBlock> *cfg, int *no_preds_pruned_count) {
+bool run(Function *function, int *no_preds_pruned_count) {
+    // Build CFG
+    // TODO: Move this into an analysis pass
+    Graph<BasicBlock> cfg;
+    cfg.set_entry(function->entry());
+    for (auto &block : *function) {
+        for (auto &inst : *block) {
+            // TODO: Make Edge<V> the default template arg for connect
+            if (auto *branch = inst->as<BranchInst>()) {
+                cfg.connect<Edge<BasicBlock>>(branch->parent(), branch->dst());
+            } else if (auto *cond_branch = inst->as<CondBranchInst>()) {
+                cfg.connect<Edge<BasicBlock>>(cond_branch->parent(), cond_branch->false_dst());
+                cfg.connect<Edge<BasicBlock>>(cond_branch->parent(), cond_branch->true_dst());
+            }
+        }
+    }
+
     bool changed = false;
     for (auto &block : *function) {
         // Remove blocks with no predecessors. This can happen after running, for example, the ConstantBranchEvaluator
         // pass where conditional branches (with two basic block destinations) are promoted into unconditional branches
         // (with only one basic block destination).
-        if (cfg->preds_of(block.get()).empty()) {
+        if (cfg.preds_of(block.get()).empty() && block.get() != cfg.entry()) {
             // Removing the block from all PHIs
             // TODO: Store the PHI users inside the basic block
             for (auto &b : *function) {
@@ -37,26 +53,10 @@ bool run(Function *function, Graph<BasicBlock> *cfg, int *no_preds_pruned_count)
 } // namespace
 
 void CfgSimplifier::run_on(Function *function) {
-    // Build CFG
-    // TODO: Move this into an analysis pass
-    Graph<BasicBlock> cfg;
-    cfg.set_entry(function->entry());
-    for (auto &block : *function) {
-        for (auto &inst : *block) {
-            // TODO: Make Edge<V> the default template arg for connect
-            if (auto *branch = inst->as<BranchInst>()) {
-                cfg.connect<Edge<BasicBlock>>(branch->parent(), branch->dst());
-            } else if (auto *cond_branch = inst->as<CondBranchInst>()) {
-                cfg.connect<Edge<BasicBlock>>(cond_branch->parent(), cond_branch->false_dst());
-                cfg.connect<Edge<BasicBlock>>(cond_branch->parent(), cond_branch->true_dst());
-            }
-        }
-    }
-
     int no_preds_pruned_count = 0;
     bool changed = false;
     do {
-        changed = run(function, &cfg, &no_preds_pruned_count);
+        changed = run(function, &no_preds_pruned_count);
     } while (changed);
     m_logger.trace("Pruned {} blocks with no predecessors", no_preds_pruned_count);
 }
