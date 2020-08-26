010ad2d6bc4cf538b70d02f66ec7af8598d8188b
Owen Smith <ojman101@gmail.com>
2020-08-26 15:55:10 +0100

    dominator-tree: Add dominates(const V *, const V *) function

--- include/bamf/graph/DominatorTree.hh
+++ include/bamf/graph/DominatorTree.hh
@@ -8,8 +8,24 @@ namespace bamf {
 
 template <typename V>
 struct DominatorTree : public Graph<V> {
+    bool dominates(const V *dominator, const V *dominatee);
     V *idom(const V *vertex);
 };
+
+// TODO: Add tests.
+template <typename V>
+bool DominatorTree<V>::dominates(const V *dominator, const V *dominatee) {
+    if (dominator == dominatee) {
+        return true;
+    }
+
+    for (const V *pred = dominatee; pred != nullptr && pred != this->entry(); pred = idom(pred)) {
+        if (pred == dominator) {
+            return true;
+        }
+    }
+    return false;
+}
 
 template <typename V>
 V *DominatorTree<V>::idom(const V *vertex) {
