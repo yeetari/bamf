a11a74e1fbb3d98a036982ac91c1cf38842167e7
Owen Smith <ojman101@gmail.com>
2020-08-29 01:36:28 +0100

    driver: Run cfg-simplifier after constant-folder

--- driver/main.cc
+++ driver/main.cc
@@ -90,8 +90,8 @@ int main(int argc, char **argv) {
         pass_manager.add<AllocPromoter>();
         pass_manager.add<TriviallyDeadInstPruner>();
         pass_manager.add<ConstantBranchEvaluator>();
-        pass_manager.add<CfgSimplifier>();
         pass_manager.add<ConstantFolder>();
+        pass_manager.add<CfgSimplifier>();
         pass_manager.add<Dumper>();
         pass_manager.run(program.get());
     } else {
