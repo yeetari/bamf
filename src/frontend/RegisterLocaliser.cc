1038a3ff370b9a37b70a3d1948ffe494c67b7c4b
Owen Smith <ojman101@gmail.com>
2020-09-07 22:04:49 +0100

    register-localiser: Remove global register

--- src/frontend/RegisterLocaliser.cc
+++ src/frontend/RegisterLocaliser.cc
@@ -5,12 +5,13 @@
 #include <bamf/ir/Function.hh>
 #include <bamf/ir/GlobalVariable.hh>
 #include <bamf/ir/Instructions.hh>
+#include <bamf/ir/Program.hh>
 
 #include <unordered_map>
 
 namespace bamf {
 
-void RegisterLocaliser::run_on(Program *) {
+void RegisterLocaliser::run_on(Program *program) {
     for (auto *reg : m_decomp_ctx.phys_regs()) {
         std::unordered_map<Function *, AllocInst *> alloc_map;
         for (auto *user : reg->users()) {
@@ -24,12 +25,12 @@ void RegisterLocaliser::run_on(Program *) {
             if (!alloc_map.contains(function)) {
                 auto *alloc = entry->prepend<AllocInst>();
                 alloc->set_name(reg->name());
-                alloc_map[function] = alloc;
+                alloc_map.emplace(function, alloc);
             }
-
             // TODO: This isn't right, this should be `inst->replace_uses_of_with(reg, alloc_map[function])`
-            reg->replace_all_uses_with(alloc_map[function]);
+            reg->replace_all_uses_with(alloc_map.at(function));
         }
+        program->remove_global(reg);
     }
 }
 
