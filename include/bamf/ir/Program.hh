654749c96123de2c9726ef91e22b841aa69b9813
Owen Smith <ojman101@gmail.com>
2020-09-07 22:04:09 +0100

    ir: Add Program::remove_global() function

--- include/bamf/ir/Program.hh
+++ include/bamf/ir/Program.hh
@@ -8,6 +8,8 @@
 #include <bamf/support/NonCopyable.hh>
 #include <bamf/support/NonMovable.hh>
 
+#include <algorithm>
+#include <cassert>
 #include <memory>
 #include <vector>
 
@@ -25,7 +27,17 @@ public:
     Program() = default;
     ~Program() = default;
 
+    // TODO: Move to source file.
     GlobalVariable *add_global() { return m_globals.emplace_back(new GlobalVariable).get(); }
+    void remove_global(GlobalVariable *global) {
+        assert(global->users().empty());
+        auto it = std::find_if(m_globals.begin(), m_globals.end(), [global](auto &ptr) {
+            return ptr.get() == global;
+        });
+        if (it != m_globals.end()) {
+            m_globals.erase(it);
+        }
+    }
 
     template <typename... Args>
     Function *create_function(Args &&... args) {
