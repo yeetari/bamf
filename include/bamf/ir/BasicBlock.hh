b8de0454a0e54cd5cb0d583d20586e7b5e92ee9b
Owen Smith <ojman101@gmail.com>
2020-09-12 17:34:47 +0100

    ir: Cleanup BasicBlock

--- include/bamf/ir/BasicBlock.hh
+++ include/bamf/ir/BasicBlock.hh
@@ -26,24 +26,14 @@ public:
     BAMF_MAKE_NON_MOVABLE(BasicBlock)
 
     BasicBlock() = default;
+    ~BasicBlock() override = default;
 
     template <typename Inst, typename... Args>
-    Inst *insert(const_iterator position, Args &&... args) requires std::derived_from<Inst, Instruction> {
-        auto *inst = new Inst(std::forward<Args>(args)...);
-        inst->set_parent(this);
-        m_instructions.emplace(position, inst);
-        return inst;
-    }
-
+    Inst *insert(const_iterator position, Args &&... args) requires std::derived_from<Inst, Instruction>;
     template <typename Inst, typename... Args>
-    Inst *prepend(Args &&... args) requires std::derived_from<Inst, Instruction> {
-        return insert<Inst>(m_instructions.begin(), std::forward<Args>(args)...);
-    }
-
+    Inst *prepend(Args &&... args) requires std::derived_from<Inst, Instruction>;
     template <typename Inst, typename... Args>
-    Inst *append(Args &&... args) requires std::derived_from<Inst, Instruction> {
-        return insert<Inst>(m_instructions.end(), std::forward<Args>(args)...);
-    }
+    Inst *append(Args &&... args) requires std::derived_from<Inst, Instruction>;
 
     const_iterator position_of(Instruction *inst) const;
     const_iterator remove(Instruction *inst);
@@ -51,7 +41,25 @@ public:
 
     void set_parent(Function *parent) { m_parent = parent; }
     Function *parent() const { return m_parent; }
-    std::size_t size() const { return m_instructions.size();}
+    std::size_t size() const { return m_instructions.size(); }
 };
+
+template <typename Inst, typename... Args>
+Inst *BasicBlock::insert(const_iterator position, Args &&... args) requires std::derived_from<Inst, Instruction> {
+    auto *inst = new Inst(std::forward<Args>(args)...);
+    inst->set_parent(this);
+    m_instructions.emplace(position, inst);
+    return inst;
+}
+
+template <typename Inst, typename... Args>
+Inst *BasicBlock::prepend(Args &&... args) requires std::derived_from<Inst, Instruction> {
+    return insert<Inst>(m_instructions.begin(), std::forward<Args>(args)...);
+}
+
+template <typename Inst, typename... Args>
+Inst *BasicBlock::append(Args &&... args) requires std::derived_from<Inst, Instruction> {
+    return insert<Inst>(m_instructions.end(), std::forward<Args>(args)...);
+}
 
 } // namespace bamf
